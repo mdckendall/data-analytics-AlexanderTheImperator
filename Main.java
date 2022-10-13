5637ede99ccf0c0dc85236c8b8cd14d13e629326
Alexander Romero <96679169+AlexanderTheImperator@users.noreply.github.com>
2022-10-13 05:23:00 +0000

    Print

--- Main.java
+++ Main.java
@@ -18,12 +18,11 @@ class Main {
 		}
 
     do {
-			System.out.println( "Press 1 to learn about salary." + "\n" +
-                          "Press 2 to learn about the job." + "\n" +
-                          "Press 3 to learn about demand." + "\n" +
-                          "Press 4 to view current students." + "\n" +
-                          "Press 5 to quit."
-                        ); 
+      System.out.println("Press 1 to learn about salary.");
+			System.out.println("Press 2 to learn about the job.");
+			System.out.println("Press 3 to learn about demand.");
+			System.out.println("Press 4 to view current students.");
+			System.out.println("Press 5 to quit.");
 			char input = Scanner.next().charAt(0);
 			if(input == '1'){
         System.out.println("1$ per hour");
